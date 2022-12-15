70f608ac5f364264f0b5729e85ba50ea86d4b560
rizzos <shinichirizzo@outlook.com>
2022-12-14 21:42:10 -0500

    Added column text to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,23 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome To Whodo</h1>
+      <div className="columnContainer">
+        <div className="column">
+          <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            vel tortor pharetra diam porta rhoncus non id nibh. Aliquam vitae
+            tortor tristique, accumsan ligula quis, fermentum leo. In non sapien
+            non tortor blandit efficitur. In ligula justo, feugiat nec tellus
+            sed, consectetur ultrices magna. In cursus, mauris et porta
+            ultrices, libero nunc bibendum odio, eu facilisis enim turpis id
+            lorem. Phasellus massa ipsum, hendrerit vitae metus quis, efficitur
+            sodales purus. Etiam rutrum hendrerit ex, ut congue ex auctor non.
+            Sed interdum orci dui, nec porttitor enim malesuada nec. Proin nibh
+            orci, porta feugiat cursus a, fringilla a nisl. Fusce euismod
+            consequat sapien, eu rutrum odio.
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
