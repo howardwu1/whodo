7c8c318cb1195e84904ab3f982f02eb7caa586fc
howard23 <howard@radiusxr.com>
2022-12-20 21:27:36 -0600

    got forgot password front-end placeholder and screen working

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Login = () => {
   const {
@@ -13,9 +13,11 @@ const Login = () => {
 
   const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const [hasForgottenPassword, setHasForgottenPassword] = useState(false);
+  const [isPasswordResetSent, setIsPasswordResetSent] = useState(false);
+  const [username, setUsername] = useState("");
 
+  const onSubmit = (data) => {
     if (data.name === "asdf" && data.password === "asdf") {
       navigate("/dashboard");
     } else {
@@ -25,52 +27,98 @@ const Login = () => {
     }
   };
 
-  return (
-    <div style={{ paddingLeft: "2em", paddingRight: "2em" }}>
-      <form onSubmit={handleSubmit(onSubmit)} className="sign-in">
-        <h2> Sign In </h2>
-        <h5> Sign in to use WhoDo project management and billing</h5>
-        <label htmlFor="name">Username or Email</label>
-        <input
-          {...register("name", { required: true })}
-          id="name"
-          onClick={() => {
-            clearErrors("signin");
-          }}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          {...register("password", { required: true })}
-          id="password"
-          type="password"
-          onClick={() => {
-            clearErrors("signin");
-          }}
-        />
-        {errors.name && (
-          <span className="login-error">Username is required</span>
-        )}
-        {errors.password && (
-          <span className="login-error">Password is required</span>
-        )}
-        {errors.signin && (
-          <span className="login-error">Username or Password is incorrect</span>
-        )}
-        <input type="submit" className="submit-btn" value="SIGN IN" />
-        <a style={{ marginBottom: "20px", marginTop: "-20px" }} href="#">
-          Forgot Password?
-        </a>
-        {/* <p>Username</p>
-        <input value={userName} onChange={(e) => setUserName(e.target.value)} />
-        <p>Password</p>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        /> */}
-      </form>
-    </div>
-  );
+  const onSubmitForgotPassword = (data) => {
+    console.log("this is a placholder");
+    setUsername(data.name);
+    setIsPasswordResetSent(true);
+  };
+
+  if (!hasForgottenPassword) {
+    return (
+      <div style={{ paddingLeft: "2em", paddingRight: "2em" }}>
+        <form onSubmit={handleSubmit(onSubmit)} className="sign-in">
+          <h2> Sign In </h2>
+          <h5> Sign in to use WhoDo project management and billing</h5>
+          <label htmlFor="name">Username or Email</label>
+          <input
+            {...register("name", { required: true })}
+            id="name"
+            onClick={() => {
+              clearErrors("signin");
+            }}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            {...register("password", { required: true })}
+            id="password"
+            type="password"
+            onClick={() => {
+              clearErrors("signin");
+            }}
+          />
+          {errors.name && (
+            <span className="login-error">Username or email is required</span>
+          )}
+          {errors.password && (
+            <span className="login-error">Password is required</span>
+          )}
+          {errors.signin && (
+            <span className="login-error">
+              Username or Password is incorrect
+            </span>
+          )}
+          <input type="submit" className="submit-btn" value="SIGN IN" />
+          <Link
+            style={{ marginBottom: "20px", marginTop: "-20px" }}
+            onClick={() => setHasForgottenPassword(true)}
+          >
+            Forgot Password?
+          </Link>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ paddingLeft: "2em", paddingRight: "2em" }}>
+        <form
+          onSubmit={handleSubmit(onSubmitForgotPassword)}
+          className="sign-in"
+        >
+          <h2> Forgot Password </h2>
+          <label htmlFor="name">Username or Email</label>
+
+          {!isPasswordResetSent && username === "" ? (
+            <>
+              <input {...register("name", { required: true })} id="name" />
+              {errors.name && (
+                <span className="login-error">
+                  Username or email is required
+                </span>
+              )}
+              <input
+                type="submit"
+                className="submit-btn"
+                value="SEND RESET EMAIL"
+              />
+            </>
+          ) : (
+            <>
+              <h3>{username}</h3>
+              <p style={{ textAlign: "left" }}>
+                Thank you! If user/email "{username}" is in our systems we have
+                sent you an email with instructions on how to reset your
+                password! Please reach out to{" "}
+                <a href="mailto:whodo-support@gmail.com">
+                  whodo-support@gmail.com
+                </a>{" "}
+                if there are any issues.
+              </p>
+            </>
+          )}
+        </form>
+      </div>
+    );
+  }
 };
 
 export default Login;
