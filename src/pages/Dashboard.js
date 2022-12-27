843c444e0340e6d3a8f51e8a44d11284b5c2e49a
howard23 <howard@radiusxr.com>
2022-12-26 20:20:15 -0600

    got dashboard tabs in and fully styled

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,37 @@
 import { AppContext } from "../App";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
+
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import "../App.css";
 
 const Dashboard = () => {
+  const tabPanelDivRef = useRef();
+
   const { username } = useContext(AppContext);
-  return <h1>Dashboard {username}</h1>;
+  return (
+    <Tabs>
+      <TabList>
+        <Tab
+          style={{
+            marginLeft: tabPanelDivRef.current.getBoundingClientRect().x + "px",
+          }}
+        >
+          Projects
+        </Tab>
+        <Tab disabled>Workspaces (Paid Feature)</Tab>
+      </TabList>
+
+      <div ref={tabPanelDivRef} style={{ maxWidth: "800px", margin: "auto" }}>
+        <TabPanel>
+          <h2>Projects {username}</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Paid only</h2>
+        </TabPanel>
+      </div>
+    </Tabs>
+  );
 };
 
 export default Dashboard;
