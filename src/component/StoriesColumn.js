273c72d91cfb6a21a3cfecefa715a0b1de513f15
howard23 <howard@radiusxr.com>
2023-01-15 16:35:56 -0600

    got bug fixed so drag and drop moves expanded details

--- src/component/StoriesColumn.js
+++ src/component/StoriesColumn.js
@@ -11,66 +11,66 @@ const StoriesColumn = ({
   columnName,
   draggable = true,
 }) => {
-  const [showStoryArr, setShowStoryArr] = useState([]);
+  const [showStoryMap, setShowStoryMap] = useState(new Map());
 
   useEffect(() => {
-    setShowStoryArr(storyList.map(() => false));
+    storyList.forEach((story) =>
+      setShowStoryMap(new Map(showStoryMap.set(story.id, false)))
+    );
   }, []);
 
-  return (
-    <div
-      className="stories-column"
-      style={{
-        marginRight: numColumns < 3 ? null : "2px",
-        marginLeft: numColumns < 3 ? null : "2px",
-        minWidth: numColumns < 3 ? 80 / numColumns - 1 + "vw" : "30vw",
-      }}
-    >
+  if (showStoryMap.size === storyList.length) {
+    return (
       <div
+        className="stories-column"
         style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginLeft: "4px",
-          marginRight: "4px",
+          marginRight: numColumns < 3 ? null : "2px",
+          marginLeft: numColumns < 3 ? null : "2px",
+          minWidth: numColumns < 3 ? 80 / numColumns - 1 + "vw" : "30vw",
         }}
       >
-        <h3>{columnName}</h3>
-        <span
+        <div
           style={{
             display: "flex",
             flexDirection: "row",
-            alignItems: "center",
+            justifyContent: "space-between",
+            marginLeft: "4px",
+            marginRight: "4px",
           }}
         >
-          <button onClick={() => setShowStoryList(false)}>X</button>
-        </span>
-      </div>
-      <div
-        style={{
-          overflowY: "auto",
-          backgroundColor: "#eee",
-          height: "100%",
-        }}
-      >
-        <DragDropContext onDragEnd={handleDrop}>
-          <Droppable droppableId="list-container">
-            {(provided) => (
-              <div
-                className="list-container"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {storyList.map((story, index) =>
-                  story.assignee === (filterBy[0] ?? story.assignee) ? (
-                    !showStoryArr[index] ? (
+          <h3>{columnName}</h3>
+          <span
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <button onClick={() => setShowStoryList(false)}>X</button>
+          </span>
+        </div>
+        <div
+          style={{
+            overflowY: "auto",
+            backgroundColor: "#eee",
+            height: "100%",
+          }}
+        >
+          <DragDropContext onDragEnd={handleDrop}>
+            <Droppable droppableId="list-container">
+              {(provided) => (
+                <div
+                  className="list-container"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {storyList.map((story, index) =>
+                    story.assignee === (filterBy[0] ?? story.assignee) ? (
                       <div
                         onClick={() => {
-                          setShowStoryArr([
-                            ...showStoryArr.slice(0, index),
-                            true,
-                            ...showStoryArr.slice(index + 1),
-                          ]);
+                          setShowStoryMap(
+                            new Map(showStoryMap.set(story.id, true))
+                          );
                         }}
                       >
                         <Draggable
@@ -79,125 +79,137 @@ const StoriesColumn = ({
                           index={index}
                           isDragDisabled={!draggable}
                         >
-                          {(provided) => (
-                            <div
-                              className="item-container"
-                              ref={provided.innerRef}
-                              {...provided.dragHandleProps}
-                              {...provided.draggableProps}
-                              title={
-                                draggable
-                                  ? ""
-                                  : "Use Current Iteration to change your story order"
-                              }
-                            >
-                              <span
-                                style={{
-                                  cursor: draggable
-                                    ? "all-scroll"
-                                    : "not-allowed",
-                                  display: "flex",
-                                  justifyContent: "space-between",
-                                  padding: "25px 10px",
-                                }}
+                          {(provided) =>
+                            !showStoryMap.get(story.id) ? (
+                              <div
+                                className="item-container"
+                                ref={provided.innerRef}
+                                {...provided.dragHandleProps}
+                                {...provided.draggableProps}
+                                title={
+                                  draggable
+                                    ? ""
+                                    : "Use Current Iteration to change your story order"
+                                }
                               >
-                                {story.title} ({story.assignee})
-                                <button>Just A Test</button>
-                              </span>
-                            </div>
-                          )}
+                                <span
+                                  style={{
+                                    cursor: draggable
+                                      ? "all-scroll"
+                                      : "not-allowed",
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                    padding: "25px 10px",
+                                  }}
+                                >
+                                  {story.title} ({story.assignee})
+                                  <button>Just A Test</button>
+                                </span>
+                              </div>
+                            ) : (
+                              <div
+                                className="item-container-details"
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                <span
+                                  style={{
+                                    display: "flex",
+                                    flexDirection: "row",
+                                  }}
+                                >
+                                  <h3
+                                    onClick={(e) => {
+                                      setStorylist([
+                                        ...storyList.slice(0, index),
+                                        {
+                                          ...story,
+                                          type: story.type,
+                                          points: story.points,
+                                        },
+                                        ...storyList.slice(index + 1),
+                                      ]);
+                                      setShowStoryMap(
+                                        new Map(
+                                          showStoryMap.set(story.id, false)
+                                        )
+                                      );
+                                      e.stopPropagation();
+                                    }}
+                                    className="text-button"
+                                  >
+                                    ^
+                                  </h3>
+                                  {story.title}
+                                </span>
+                                <form onClick={(e) => e.stopPropagation()}>
+                                  <div className="story-group">
+                                    <label htmlFor="story-type">
+                                      STORY TYPE
+                                    </label>
+                                    <select
+                                      id="story-type"
+                                      defaultValue={story.type}
+                                      onInput={(e) =>
+                                        (story.type = e.target.value)
+                                      }
+                                    >
+                                      <option value="feature">Feature</option>
+                                      <option value="bug">Bug</option>
+                                      <option value="chore">Chore</option>
+                                      <option value="release">Release</option>
+                                    </select>
+                                  </div>
+
+                                  <div className="point-system">
+                                    <label htmlFor="point-input">POINTS</label>
+                                    <select
+                                      id="point-input"
+                                      defaultValue={story.points}
+                                      onInput={(e) =>
+                                        (story.points = e.target.value)
+                                      }
+                                    >
+                                      <option value="unestimated">
+                                        unestimated
+                                      </option>
+                                      <option value="0">0 Points</option>
+                                      <option value="1">1 Points</option>
+                                      <option value="2">2 Points</option>
+                                      <option vaule="3">3 Points</option>
+                                    </select>
+                                  </div>
+                                </form>
+                              </div>
+                            )
+                          }
                         </Draggable>
                       </div>
-                    ) : (
-                      <div
+                    ) : null
+                  )}
+                  {storyList.length === 0 ? (
+                    <>
+                      <ContentPlaceholder color={"#ccc"} />
+                      <h3
                         style={{
-                          height: "500px",
-                          backgroundColor: "grey",
-                          border: "1px solid gold",
-                          margin: "5px 30px",
-                          padding: "2px 10px",
-                        }}
-                        onClick={() => {
-                          setShowStoryArr([
-                            ...showStoryArr.slice(0, index),
-                            false,
-                            ...showStoryArr.slice(index + 1),
-                          ]);
+                          margin: "10% 10% 0% 10%  ",
                         }}
                       >
-                        <span style={{ display: "flex", flexDirection: "row" }}>
-                          <h3
-                            onClick={() =>
-                              setStorylist([
-                                ...storyList.slice(0, index),
-                                {
-                                  ...story,
-                                  type: story.type,
-                                  points: story.points,
-                                },
-                                ...storyList.slice(index + 1),
-                              ])
-                            }
-                            className="text-button"
-                          >
-                            ^
-                          </h3>
-                          {story.title}
-                        </span>
-                        <form onClick={(e) => e.stopPropagation()}>
-                          <div className="story-group">
-                            <label htmlFor="story-type">STORY TYPE</label>
-                            <select
-                              id="story-type"
-                              defaultValue={story.type}
-                              onInput={(e) => (story.type = e.target.value)}
-                            >
-                              <option value="feature">Feature</option>
-                              <option value="bug">Bug</option>
-                              <option value="chore">Chore</option>
-                              <option value="release">Release</option>
-                            </select>
-                          </div>
-
-                          <div className="point-system">
-                            <label htmlFor="point-input">POINTS</label>
-                            <select
-                              id="point-input"
-                              defaultValue={story.points}
-                              onInput={(e) => (story.points = e.target.value)}
-                            >
-                              <option value="unestimated">unestimated</option>
-                              <option value="0">0 Points</option>
-                              <option value="1">1 Points</option>
-                              <option value="2">2 Points</option>
-                              <option vaule="3">3 Points</option>
-                            </select>
-                          </div>
-                        </form>
-                      </div>
-                    )
-                  ) : null
-                )}
-                {storyList.length === 0 ? (
-                  <>
-                    <ContentPlaceholder color={"#ccc"} />
-                    <h3
-                      style={{
-                        margin: "10% 10% 0% 10%  ",
-                      }}
-                    >
-                      Nothing here yet. Add stories to this list!
-                    </h3>
-                  </>
-                ) : null}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
+                        Nothing here yet. Add stories to this list!
+                      </h3>
+                    </>
+                  ) : null}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <div>Loading...</div>;
 };
 
 export default StoriesColumn;
